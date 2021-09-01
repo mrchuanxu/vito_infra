4380f569970108efcfb186a2552f36801559f885
Mr.Trans <vitomychueng@163.com>
2021-09-01 15:01:02 +0800

    rm alegric things

--- common/etcd_get_set_test.go
+++ common/etcd_get_set_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Test_GetSet(t *testing.T){
-	_,err :=common.EtcdClientKV.Put(context.Background(),"redis_addr","jinmao_cn:2Nc^JeNS7Jjm0755@tcp(pc-2zet0b02gcf3ylb32-out.mysql.polardb.rds.aliyuncs.com:3306)/")
+	_,err :=common.EtcdClientKV.Put(context.Background(),"redis_addr","")
 	assert.Nil(t, err)
 	kvrsp,err := common.EtcdClientKV.Get(context.Background(),"dbConnStr")
 	t.Log(string(kvrsp.Kvs[0].Value))
